46deb48d5b4f03c07454f7a7ebb1b7e74791d808
KauanMonteiro <kauanmonteirosilva789@gmail.com>
2024-01-09 13:30:16 -0300

    update

--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'recipes/home.html')
