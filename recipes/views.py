577335e978c0621782222a0244349a0573ade25f
KauanMonteiro <kauanmonteirosilva789@gmail.com>
2024-01-28 15:42:55 -0300

    update Category

--- recipes/views.py
+++ recipes/views.py
@@ -5,16 +5,17 @@ from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter( is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
+        is_published=True,
         category__id=category_id
     ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
