497d90522b8c11105e66d877b98c6ab34497ded9
KauanMonteiro <kauanmonteirosilva789@gmail.com>
2024-07-01 10:26:12 -0300

    Refatoração das views home e recipe para usar get_object_or_404 do Django

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404,get_list_or_404
 from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
@@ -6,6 +6,7 @@ from recipes.models import Recipe
 
 def home(request):
     recipes = Recipe.objects.filter( is_published=True).order_by('-id')
+    
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -22,10 +23,7 @@ def category(request, category_id):
     })
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        is_published=True,
-        pk = id
-    ).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
