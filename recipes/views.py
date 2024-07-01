e174900e76601152aef2bf64edb3d8ff6413f00f
KauanMonteiro <kauanmonteirosilva789@gmail.com>
2024-07-01 09:44:10 -0300

    Substituída a consulta direta por `get_list_or_404` no view `category` para garantir tratamento adequado de consultas. Introduzido título dinâmico no template `category` para exibir o nome da categoria selecionada.

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
@@ -11,12 +11,14 @@ def home(request):
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        is_published=True,
-        category__id=category_id
-    ).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(
+         is_published=True,
+         category__id=category_id
+    ).order_by('-id'))
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+         'title': f'{recipes[0].category.name}-Category'
     })
 
 def recipe(request, id):
